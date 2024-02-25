db20a7b151cbfa1d12478d32f54cf247128f6e58
DimaM83 <mdv16.01.83@gmail.com>
2024-02-25 12:35:10 +0100

    hw-5

--- js/task-2.js
+++ js/task-2.js
@@ -1,36 +1,13 @@
-function calcAverageCalories(days) {
-  if (days.length === 0) {
-    return 0;
+function makeArray(firstArray, secondArray, maxLength) {
+  const newArray = [...firstArray, ...secondArray];
+  if (newArray.length > maxLength) {
+    return newArray.slice(0, maxLength);
   }
-
-  const totalCalories = days.reduce((acc, day) => acc + day.calories, 0);
-  return totalCalories / days.length;
+  return newArray;
 }
 
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 3010 },
-    { day: "tuesday", calories: 3200 },
-    { day: "wednesday", calories: 3120 },
-    { day: "thursday", calories: 2900 },
-    { day: "friday", calories: 3450 },
-    { day: "saturday", calories: 3280 },
-    { day: "sunday", calories: 3300 }
-  ])
-); // 3180
-
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 2040 },
-    { day: "tuesday", calories: 2270 },
-    { day: "wednesday", calories: 2420 },
-    { day: "thursday", calories: 1900 },
-    { day: "friday", calories: 2370 },
-    { day: "saturday", calories: 2280 },
-    { day: "sunday", calories: 2610 }
-  ])
-); // 2270
-
-console.log(
-  calcAverageCalories([])
-); // 0
+console.log(makeArray(['Манго', 'Поли'], ['Аякс', 'Челси'], 3));
+console.log(makeArray(['Манго', 'Поли', 'Хьюстон'], ['Аякс', 'Челси'], 4));
+console.log(makeArray(['Манго'], ['Аякс', 'Челси', 'Поли', 'Хьюстон'], 3));
+console.log(makeArray(['Земля', 'Юпитер'], ['Нептун', 'Уран'], 2));
+console.log(makeArray(['Земля', 'Юпитер'], ['Нептун', 'Уран', 'Венера'], 0));
